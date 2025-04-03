a6b80f32c2533b420be0e791656f071099e0799a
Sudhanshu Kumar <tiwarisunny7352@gmail.com>
2025-04-03 12:25:28 +0530

    add app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-     <div>
-     <Header/>
-     </div>
-        
-    </>
+   <main>
+    vite app
+   </main>
   )
 }
 
